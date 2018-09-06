2e9de5f7113a828f3b60b1917d745d2fb6260344
pmcquay <patrick@betterimpact.com>
2018-09-06 12:14:41 -0400

    named the timeclock qr code property more appropriately.

--- VolunteerSquared.ApiClient/Models/User.cs
+++ VolunteerSquared.ApiClient/Models/User.cs
@@ -77,8 +77,8 @@ namespace VolunteerSquared.ApiClient.Models
         public string PhotoUrlScaled { get; set; }
         [JsonProperty("photo_url_original")]
         public string PhotoUrlOriginal { get; set; }
-        [JsonProperty("qr_code_url")]
-        public string QRCodeUrl { get; set; }
+        [JsonProperty("timeclock_qr_code_url")]
+        public string TimeClockQRCodeUrl { get; set; }
 
         [JsonProperty("memberships")]
         public IList<UserOrganizationMembership> Memberships { get; set; }
